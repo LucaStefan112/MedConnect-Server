6e0b8818cea85a3eaa2c55b81d5f6b9240993f91
Luca-Stefan Tamas <lucastefantamas@gmail.com>
2023-03-08 13:09:25 +0200

    added test route

--- index.ts
+++ index.ts
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/test', (req, res) => {
+  res.send('Hello Test!');
+});
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
