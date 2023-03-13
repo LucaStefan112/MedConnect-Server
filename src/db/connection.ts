b393b94860527290e70135d77343814ec4c53c56
Achitei Marius <marius.achitei02@gmail.com>
2023-03-13 17:50:22 +0200

    connected to db using mongoose
    moved connection username and password to a separate dotenv file

---
+++ src/db/connection.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const dotenv = require("dotenv");
+
+export function dbConnect() {
+  if (dotenv.error) {
+    throw dotenv.error;
+  }
+
+  const { USERNAME, PASSWORD } = dotenv.config().parsed;
+
+  const connectionString =
+    "mongodb+srv://" +
+    USERNAME +
+    ":" +
+    PASSWORD +
+    "@db.i0bozci.mongodb.net/?retryWrites=true&w=majority";
+
+  mongoose
+    .connect(connectionString)
+    .then(() => {
+      console.log("Baza e sus, la dispozitia dvs.");
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log("Baza e jos, verifica cablajele!");
+    });
+}
