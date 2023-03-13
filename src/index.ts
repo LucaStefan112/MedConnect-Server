f142914ef28352d335de83ba700f0833c77a8e9a
Achitei Marius <marius.achitei02@gmail.com>
2023-03-13 11:47:10 +0200

    Deleted testSchema and test-routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 const express = require("express");
 import mongoose from "mongoose";
 
+const username = "M2";
+const password = "Bvq0cLqzUadzKcBF";
 const connectionString =
-  "mongodb+srv://M2:Bvq0cLqzUadzKcBF@db.i0bozci.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://" +
+  username +
+  ":" +
+  password +
+  "@db.i0bozci.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(connectionString)
@@ -14,42 +20,10 @@ mongoose
     console.log("Baza e jos, verifica cablajele!");
   });
 
-const Schema = mongoose.Schema;
-
-const testSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    default: "-",
-  },
-  added: Date,
-});
-
-const Test = mongoose.model("Test", testSchema);
-
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-
-app.get("/test-db/add", async (req, res) => {
-  const test = new Test({
-    name: "Test",
-    added: Date.now(),
-  });
-  await test.save();
-  res.send(test);
-});
-
-app.get("/test-db/show", async (req, res) => {
-  const tests = await Test.find();
-  res.send(tests);
-});
-
-app.get("/test-db-clear", async (req, res) => {
-  await Test.deleteMany();
-  const tests = await Test.find();
-  res.send(tests);
 });
 
 app.get("/users", (req, res) => {
