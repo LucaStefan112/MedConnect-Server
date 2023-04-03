540986f1cf45501d90e5c100be95f28c5581d814
Luca-Stefan Tamas <lucastefantamas@gmail.com>
2023-04-03 17:43:18 +0300

    added cors middleware

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,12 @@ import appointmentRouter from "./routes/appointment";
 import userRouter from "./routes/user";
 import analysisRouter from "./routes/analysis";
 import { checkAuth } from "./controllers/check-auth.controller";
+import corsMiddleware from "./middlewares/cors";
 
 const express = require("express");
 const app = express();
 
+app.use(corsMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
