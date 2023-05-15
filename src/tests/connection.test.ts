9bb29f312d28b224ed59c89e0abac746a88c480b
miruna-iosub <mirunaelenaiii2403@gmail.com>
2023-05-15 19:54:49 +0300

    testing

--- src/tests/connection.test.ts
+++ src/tests/connection.test.ts
@@ -1,20 +1,61 @@
 import mongoose from "mongoose";
 import { dbConnect } from "../db/connection";
 
+jest.mock("mongoose", () => ({
+  connect: jest.fn(),
+}));
+
+jest.mock("dotenv", () => ({
+  config: jest.fn(() => ({
+    parsed: { USERNAME: "testuser", PASSWORD: "testpassword" },
+  })),
+  error: null,
+}));
+
+
 describe("dbConnect", () => {
-  it("should connect to the database successfully", async () => {
-    const logSpy = jest.spyOn(console, 'log');
-
-    await dbConnect();
-
-    expect(logSpy).toHaveBeenCalledWith('Successfully connected to database');
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  it("should throw an error if the connection fails", async () => { //if you want to see this test fail, change the info in .env to something else
-    const logSpy = jest.spyOn(console, 'log');
+  it("should connect to the database with the provided connection string", async () => {
+    const mockConnect = mongoose.connect as jest.Mock;
+    const connectionString =
+      "mongodb+srv://testuser:testpassword@db.example.com";
 
     await dbConnect();
 
-    expect(logSpy).toHaveBeenCalledWith('Failed to connect to database!');
+    expect(mockConnect).toHaveBeenCalledWith(connectionString);
+    expect(console.log).toHaveBeenCalledWith(
+      "Successfully connected to database"
+    );
+  });
+
+  it("should throw an error if there is an error during database connection", async () => {
+    const mockConnect = mongoose.connect as jest.Mock;
+    const error = new Error("Database connection failed");
+
+    mockConnect.mockRejectedValue(error);
+
+    await expect(dbConnect()).rejects.toThrow(error);
+
+    expect(mockConnect).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith(error);
+    expect(console.log).toHaveBeenCalledWith("Failed to connect to database!");
+  });
+
+  it("should throw an error if dotenv.config throws an error", async () => {
+    jest.mock("dotenv", () => ({
+      config: jest.fn(() => {
+        throw new Error("dotenv config error");
+      }),
+      error: new Error("dotenv config error"),
+    }));
+
+    const mockConnect = mongoose.connect as jest.Mock;
+
+    await expect(dbConnect()).rejects.toThrow(new Error("dotenv config error"));
+
+    expect(mockConnect).not.toHaveBeenCalled();
   });
 });
